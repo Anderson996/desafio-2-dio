5e88dc288d114619356169079905852ff9280a3f
Anderson996 <99ldoand@gmail.com>
2024-10-14 00:50:36 -0300

    Resultado final!

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-aaaa
+//total de vitórias e derrotas
+let totalWin = winningCounter(80 , 5)
+
+//ranques possíveis
+let rank = ["ferro" , "bronze" , "prata" , "ouro" , "diamante" , "lendário" , "imortal"]
+
+//função de contabilização de vitórias e derrotas (vai ficar aqui!!)
+function winningCounter(win , loss){
+    let average = win - loss
+    return average
+}
+
+//Vitórias por ranque
+if(totalWin <= 10){
+    rank = rank[0]
+}
+else if(totalWin > 10 && totalWin <= 20){
+    rank = rank[1]
+}
+else if(totalWin > 20 && totalWin <= 50){
+    rank = rank[2]
+}
+else if(totalWin > 50 && totalWin <= 80){
+    rank = rank[3]
+}
+else if(totalWin > 80 && totalWin <= 90){
+    rank = rank[4]
+}
+else if(totalWin > 90 && totalWin <= 100){
+    rank = rank[5]
+}
+else if(totalWin > 100){
+    rank = rank[6]
+}
+
+console.log(`O herói tem saldo de ${totalWin} partidas e está no nível ${rank}`)
